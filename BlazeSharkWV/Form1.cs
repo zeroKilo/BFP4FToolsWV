c6d840a2c066715d1817b76f36708959f65c0086
Jan Altensen <info@stricted.net>
2019-03-04 21:17:56 +0100

    allow loading multiple packet logs
    Change-Id: Iee528f567c98fdcdb0b633f08b15a33111adc685

--- BlazeSharkWV/Form1.cs
+++ BlazeSharkWV/Form1.cs
@@ -465,14 +465,18 @@ namespace BlazeSharkWV
 
             OpenFileDialog d = new OpenFileDialog();
             d.Filter = "*.bin|*.bin";
+            d.Multiselect = true;
             if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                byte[] data = File.ReadAllBytes(d.FileName);
-                lock (_sync)
+                packets.Clear();
+                foreach (String file in d.FileNames)
                 {
-                    packets.Clear();
-                    packets.AddRange(Blaze.FetchAllBlazePackets(new MemoryStream(data)));
-                    packetCount = -1;
+                    byte[] data = File.ReadAllBytes(file);
+                    lock (_sync)
+                    {
+                        packets.AddRange(Blaze.FetchAllBlazePackets(new MemoryStream(data)));
+                        packetCount = -1;
+                    }
                 }
             }
         }
